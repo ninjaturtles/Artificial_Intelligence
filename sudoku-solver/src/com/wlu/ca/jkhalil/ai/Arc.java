a06e2bf03970d21bad050d9616fa03faf3e07507
Johnny Khalil <khal0988@mylaurier.ca>
2017-11-10 15:29:21 -0500

    initial commit - CSP sudoku solver

---
+++ sudoku-solver/src/com/wlu/ca/jkhalil/ai/Arc.java
@@ -0,0 +1,29 @@
+package com.wlu.ca.jkhalil.ai;
+
+/**
+ * file name: Arc.java
+ * Arc class implementation
+ * 
+ * @author Akanksha Malik - Johnny Khalil
+ * @since Nov 6th, 2017
+ */
+public class Arc {
+    public Variable X;
+    public Variable Y;
+
+    /**
+     * Arc constructor, initializes an arc between two variables
+     * 
+     * @param x	- first variable in the arc
+     * @param y	- second variable in the arc
+     */
+    public Arc(Variable x, Variable y) {
+        this.X = x;
+        this.Y = y;
+    }
+    
+    @Override
+    public String toString() {
+    		return "("+X.row +","+X.column+")"+ "("+Y.row +","+Y.column+")" + "||"; 
+    }
+}
---
+++ sudoku-solver/src/com/wlu/ca/jkhalil/ai/Constraint.java
@@ -0,0 +1,30 @@
+package com.wlu.ca.jkhalil.ai;
+
+/**
+ * file name: Constraint.java
+ * Constraint class implementation
+ * 
+ * @author Akanksha Malik - Johnny Khalil
+ * @since Nov 6th, 2017
+ */
+public class Constraint {
+
+    public int row;
+    public int col;
+
+    /**
+     * Binary Constraint constructor
+     * Need only the position of the constraint on the grid
+     * @param rhsRow - the row of the second variable
+     * @param rhsCol - the column of the second variable
+     */
+    public Constraint(int rhsRow, int rhsCol) {
+        this.row = rhsRow;
+        this.col = rhsCol;
+    }
+    
+    @Override
+    public String toString() {
+    		return "("+row+","+col+")"; 
+    }
+} // end of Constraint
